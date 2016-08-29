0d6f24cf0a16ded3578bb36e7c680b77d90dd567
bmtober <btober@computer.org>
2016-08-29 20:47:44 +0000

    Plotting improvements

--- plot.R
+++ plot.R
@@ -1,4 +1,4 @@
 x <- read.table("document_similarity.csv", header=FALSE, sep=",")
 library('txtplot')
-txtplot(x$V2,x$V3)
+txtplot(x$V2, x$V3, xlab="Spamminess", ylab="Hamminess", xlim=c(0,1), ylim=c(0,1))
 q()
