1bf46e1769be592e654081fd1572267d8f0e2e2c
bmtober <btober@computer.org>
2016-08-31 05:43:29 -0400

    Added comment showing command to add library.

--- plot.R
+++ plot.R
@@ -1,3 +1,5 @@
+# install.packages("txtplot")
+
 library('txtplot')
 x <- read.table("document_similarity.csv", header=FALSE, sep="\t")
 txtplot(x$V2, x$V3, xlab="Spamminess", ylab="Hamminess", xlim=c(0,1), ylim=c(0,1))
