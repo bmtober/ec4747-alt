8a8639de931bb6df9ea1b20bd303e4caa0a74a27
Berend Tober <btober@laptop.btober.net>
2016-09-02 18:45:37 -0400

    Plotting updates.

--- plot_acc.R
+++ plot_acc.R
@@ -5,4 +5,9 @@ t <- read.table("top_ten_term_similarity.acc", header=FALSE, sep=" ")
 txtplot(d$V7,d$V6, ylim=c(0,1), xlab="Threshold", ylab="Accuracy")
 txtplot(a$V7,a$V6, ylim=c(0,1), xlab="Threshold", ylab="Accuracy")
 txtplot(t$V7,t$V6, ylim=c(0,1), xlab="Threshold", ylab="Accuracy")
+
+plot(d$V7,d$V6, ylim=c(0,1), xlab="Threshold", ylab="Accuracy", main="Document Frequency")
+plot(a$V7,a$V6, ylim=c(0,1), xlab="Threshold", ylab="Accuracy", main="Average Term Frequency")
+plot(t$V7,t$V6, ylim=c(0,1), xlab="Threshold", ylab="Accuracy", main="Top Ten Terms Frequency")
+
 q()
