559528b65e965d604f16e10daf44716083b21c58
bmtober <btober@computer.org>
2016-09-02 06:32:26 -0400

    Added png graphics of relative similarity plots.

--- plot.R
+++ plot.R
@@ -2,13 +2,18 @@
 
 library('txtplot')
 
-x <- read.table("document_similarity.csv", header=TRUE, sep="\t")
-txtplot(x$spamminess, x$hamminess, xlab="Spamminess", ylab="Hamminess", xlim=c(0,1), ylim=c(0,1))
+d <- read.table("document_similarity.csv", header=TRUE, sep="\t")
+a <- read.table("average_term_similarity.csv", header=TRUE, sep="\t")
+t <- read.table("top_ten_term_similarity.csv", header=TRUE, sep="\t")
 
-x <- read.table("average_term_similarity.csv", header=TRUE, sep="\t")
-txtplot(x$spamminess, x$hamminess, xlab="Spamminess", ylab="Hamminess", xlim=c(0,1), ylim=c(0,1))
+txtplot(d$spamminess, d$hamminess, xlab="Spamminess", ylab="Hamminess", xlim=c(0,1), ylim=c(0,1))
+txtplot(a$spamminess, a$hamminess, xlab="Spamminess", ylab="Hamminess", xlim=c(0,1), ylim=c(0,1))
+txtplot(t$spamminess, t$hamminess, xlab="Spamminess", ylab="Hamminess", xlim=c(0,1), ylim=c(0,1))
 
-x <- read.table("top_ten_term_similarity.csv", header=TRUE, sep="\t")
-txtplot(x$spamminess, x$hamminess, xlab="Spamminess", ylab="Hamminess", xlim=c(0,1), ylim=c(0,1))
+png()
+
+plot(d$spamminess, d$hamminess, xlab="Spamminess", ylab="Hamminess", xlim=c(0,1), ylim=c(0,1), main="Document Frequency")
+plot(a$spamminess, a$hamminess, xlab="Spamminess", ylab="Hamminess", xlim=c(0,1), ylim=c(0,1), main="Average Term Frequency")
+plot(t$spamminess, t$hamminess, xlab="Spamminess", ylab="Hamminess", xlim=c(0,1), ylim=c(0,1), main="Top Ten Terms Frequency")
 
 q()
